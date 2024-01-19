83bc8864b93ebc8ccdb04c41dae227a65e279a9d
San Andres <cool.snow@mail.ru>
2024-01-19 22:09:12 +0300

    life data - 1

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.MutableLiveData
 
 class MainActivity : AppCompatActivity() {
+
+    private val getData = GetData()
+    private val lifeDateString = MutableLiveData<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        lifeDateString.value = "Hello life data"
+        val test_text = findViewById<TextView>(R.id.test_text)
+        test_text.text = lifeDateString.value
     }
 }
