226d7186d3128e160843b15669f8b483ce01a1af
San Andres <cool.snow@mail.ru>
2024-01-20 12:24:49 +0300

    life data layout for fork

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.MutableLiveData
@@ -13,6 +15,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val editedText = findViewById<TextView>(R.id.textView)
+        val editedTextButton = findViewById<View>(R.id.EditTextViewButton)
+        val editTextLine = findViewById<TextView>(R.id.EditTextView)
+
+//        editButton.setOnClickListener {
+//            println("!!! ${myLifeData.value}")
+//            myLifeData.setValueToLiveData(editedText.text.toString())
+//            println("!!! ${myLifeData.value}")
+//        }
         myLifeData.observe(this, Observer {  })
     }
 }
--- app/src/main/java/com/example/mvvm/MyLIfeData.kt
+++ app/src/main/java/com/example/mvvm/MyLIfeData.kt
@@ -3,13 +3,17 @@ package com.example.mvvm
 import androidx.lifecycle.LiveData
 
 class MyLifeData:LiveData<String> (){
-    override fun onActive() {
-        super.onActive()
-        println("onActive")
-    }
 
-    override fun onInactive() {
-        super.onInactive()
-        println("onInActive")
+    fun setValueToLiveData(string: String) {
+        value = string
     }
+//    override fun onActive() {
+//        super.onActive()
+//        println("!!!onActive")
+//    }
+//
+//    override fun onInactive() {
+//        super.onInactive()
+//        println("!!!onInActive")
+//    }
 }
